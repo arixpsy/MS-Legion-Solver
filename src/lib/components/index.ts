7ad7b20ca8c6384ca0dd1fbb959759273a25ae3a
Phua Si Yu Arix <arixgg@gmail.com>
2024-05-09 03:08:55 +0800

    wip: create temp <LegionBoard />

---
+++ src/lib/components/index.ts
@@ -0,0 +1,2 @@
+export { default as LegionBoard } from './LegionBoard/LegionBoard.svelte'
+export { default as PlacedPiece } from './PlacedPiece/PlacedPiece.svelte'
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { LegionBoard } from '$lib/classes'
+
+	import LegionBoardComponent from '$lib/components/LegionBoard/LegionBoard.svelte'
+
+	let board = new LegionBoard(500)
+	let playerLevel: number = $state(500)
+
+	function onChangePlayerLevel() {
+		board.setBoardSize(playerLevel)
+	}
+</script>
+
+<div class="min-h-screen flex justify-center items-center flex-col">
+	<div class="flex flex-col border border-gray-400">
+		<label for="playerLevel"> Player Level:</label>
+		<input
+			id="playerLevel"
+			type="number"
+			min={500}
+			bind:value={playerLevel}
+			onchange={onChangePlayerLevel}
+		/>
+	</div>
+
+	<LegionBoardComponent {board} />
+</div>
