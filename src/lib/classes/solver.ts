efe729ec265e46f89eb7e50bca23d608cc7ab8b0
Phua Si Yu Arix <arixgg@gmail.com>
2024-05-20 01:58:35 +0800

    wip: finish brute force solve

--- src/lib/classes/solver.ts
+++ src/lib/classes/solver.ts
@@ -15,40 +15,179 @@ export default class Solver {
 		for (const [shapeType, numOfPieces] of Object.entries(shapeCountMap)) {
 			for (let num = 0; num < numOfPieces; ++num) {
 				const shape = new Shape(shapeType as ShapeType)
-				const piece = new Piece(shape)
+				const piece = new Piece({ shape })
 				pieces.push(piece)
 			}
 		}
+		pieces.sort((a, b) => b.shape.blockCount - a.shape.blockCount)
 		return pieces
 	}
 
-	solve() {
-		if (this.pieces.length === 0) throw Error('No pieces to place on the board')
-		if (this.board.blocksToFill === 0) throw Error('No selected board area')
+	getPiecesBlockCount(): number {
+		let blockCount = 0
+		for (const piece of this.pieces) {
+			blockCount += piece.shape.blockCount
+		}
+		return blockCount
+	}
 
-		// find all starting points
-		const startPoints: Array<Point> = []
+	getStartingPoints(): Array<Point> {
+		const startingPoints = []
 		for (const midPoint of this.board.middlePoints) {
 			if (this.board.selectedArea[midPoint.y][midPoint.x]) {
-				startPoints.push(midPoint)
+				startingPoints.push(midPoint)
 			}
 		}
+		return startingPoints
+	}
 
-		if (startPoints.length === 0) throw Error('Selected board area invalid.')
+	async solve() {
+		if (this.pieces.length === 0) throw Error('No pieces to place on the board')
+		if (this.board.blocksToFill === 0) throw Error('No selected board area')
+		if (this.board.blocksToFill !== this.getPiecesBlockCount())
+			throw Error('Size of selected area should match the block count of all pieces')
+
+		// find all starting points
+		const startingPoints = this.getStartingPoints()
+
+		if (startingPoints.length === 0) throw Error('Selected board area invalid.')
 
 		// loop through pieces and see if they fit the selected area for each starting point
 		const possibleFirstPlacedPieces: Array<PlacedPiece> = []
-		for (const startPoint of startPoints) {
+		for (const startPoint of startingPoints) {
 			for (const piece of this.pieces) {
-				if (this.canPlacePiece(startPoint, piece)) {
-					// TODO: need to figure out rotate for every piece
-					possibleFirstPlacedPieces.push(new PlacedPiece(startPoint, piece))
+				for (const rotation of [0, 90, 180, 270]) {
+					if (rotation && !piece.shape.canRotate()) break
+					for (const flip of [false, true]) {
+						if (flip && !piece.shape.canFlip()) break
+						const transformedPiece = new Piece({
+							id: piece.id,
+							shape: new Shape(piece.shape.shapeType, rotation, flip)
+						})
+						if (this.canPlacePiece(startPoint, transformedPiece)) {
+							possibleFirstPlacedPieces.push(new PlacedPiece(startPoint, transformedPiece))
+						}
+					}
 				}
 			}
 		}
+
+		if (possibleFirstPlacedPieces.length === 0) throw Error('Board cannot be solved')
+
+		const startTime = performance.now()
+		let isSolved = false
+
+		for (const firstPlacedPiece of possibleFirstPlacedPieces) {
+			const piecesLeft = this.pieces.filter((p) => p.id !== firstPlacedPiece.id)
+			this.board.addPlacedPiece(firstPlacedPiece)
+
+			const nextPoint = this.findNextPointToFill()
+			if (!nextPoint) {
+				isSolved = true
+				break
+			}
+
+			const pieceCombinations: Array<Piece> = []
+
+			for (const piece of piecesLeft) {
+				for (const rotation of [0, 90, 180, 270]) {
+					if (rotation && !piece.shape.canRotate()) break
+					for (const flip of [false, true]) {
+						if (flip && !piece.shape.canFlip()) break
+						const transformedPiece = new Piece({
+							id: piece.id,
+							shape: new Shape(piece.shape.shapeType, rotation, flip)
+						})
+						pieceCombinations.push(transformedPiece)
+					}
+				}
+			}
+
+			const solved = await this.fillNextPointRecursive(pieceCombinations, [], nextPoint)
+
+			if (solved) {
+				isSolved = true
+				break
+			}
+
+			this.board.removeLastPiece()
+		}
+
+		if (isSolved) {
+			console.log('solved')
+		}
+
+		const endTime = performance.now()
+		console.log(`Solution found in ${endTime - startTime} milliseconds`)
 	}
 
-	canPlacePiece(point: Point, piece: Piece) {
+	async fillNextPointRecursive(
+		pieceCombinations: Array<Piece>,
+		history: Array<PlacedPiece>,
+		point: Point | undefined
+	): Promise<boolean> {
+		if (!point) return false
+		let pieceIndex = 0
+		let currentPiece = pieceCombinations[pieceIndex]
+		let currentPoint: Point | undefined = point
+
+		while (currentPoint !== undefined) {
+			const canFitPiece = this.canPlacePiece(currentPoint, currentPiece)
+
+			if (canFitPiece) {
+				const placedPiece = new PlacedPiece(currentPoint, currentPiece)
+				this.board.addPlacedPiece(placedPiece)
+				history.push(placedPiece)
+				await this.sleep(1)
+
+				if (history.length === this.pieces.length - 1) {
+					return true
+				}
+
+				const newPieceCombinations = pieceCombinations.filter((p) => p.id !== currentPiece.id)
+
+				const newPermutation = await this.fillNextPointRecursive(
+					newPieceCombinations,
+					history,
+					this.findNextPointToFill()
+				)
+
+				if (newPermutation) {
+					return true
+				} else {
+					this.board.removeLastPiece()
+					history.pop()
+				}
+			}
+
+			if (pieceIndex < pieceCombinations.length - 1) {
+				pieceIndex = pieceIndex + 1
+				currentPiece = pieceCombinations[pieceIndex]
+			} else {
+				currentPoint = this.findNextPointToFill(currentPoint)
+				pieceIndex = 0
+				currentPiece = pieceCombinations[pieceIndex]
+			}
+		}
+
+		return false
+	}
+
+	findNextPointToFill(currentPoint?: Point): Point | undefined {
+		for (let row = 0; row < this.board.filledArea.length; row++) {
+			if (currentPoint && row < currentPoint.y) continue
+			for (let col = 0; col < this.board.filledArea[row].length; col++) {
+				if (!this.board.selectedArea[row][col]) continue
+				if (currentPoint && row <= currentPoint.y && col <= currentPoint.x) continue
+				if (!this.board.filledArea[row][col]) {
+					return new Point(col, row)
+				}
+			}
+		}
+		return undefined
+	}
+
+	canPlacePiece(point: Point, piece: Piece): boolean {
 		const shapeLayout = piece.shape.layout
 		const pieceMiddleOffset = piece.middleOffset
 
@@ -59,15 +198,25 @@ export default class Solver {
 					const boardX = point.x - (pieceMiddleOffset.x - col)
 					const boardY = point.y - (pieceMiddleOffset.y - row)
 
+					// check if point out of bounds
+					if (boardY < 0) return false
+					if (boardX < 0) return false
+					if (boardY >= this.board.selectedArea.length) return false
+					if (boardX >= this.board.selectedArea[boardY].length) return false
+
 					// check if point is filled in a selected area
 					if (!this.board.selectedArea[boardY][boardX]) return false
 
 					// check if point is filled in a filled area
-					if (this.board.state[boardY][boardX] !== 0) return false
+					if (this.board.filledArea[boardY][boardX]) return false
 				}
 			}
 		}
 
 		return true
+	}
+
+	sleep(ms: number) {
+		return new Promise((resolve) => setTimeout(resolve, ms))
 	}
 }
