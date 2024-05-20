16ed292ba84e827df4358c69986a1ddb495c71f8
Phua Si Yu Arix <arixgg@gmail.com>
2024-05-21 03:27:23 +0800

    wip: add temp legion ui

---
+++ src/lib/components/LegionUI/LegionUI.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import bg from '$lib/assets/legion-bg.png'
+</script>
+
+<div
+	class="relative flex justify-between object-cover w-[1008px] h-[743px] pt-[9px] pr-[13px] pb-[19px] pl-[13px]"
+	style:background={`url(${bg})`}
+>
+	<!-- Left Side -->
+	<div class="w-[582px] flex flex-col">
+		<div class="w-[582px] h-[478px]">
+      <!-- Legion Board -->
+      <slot name='legion-board' />
+    </div>
+	</div>
+
+	<!-- Right Side -->
+
+	<div class="bg-green-600 w-[392px] opacity-20"></div>
+
+	<!-- <div class=" px-[13px] py-[8px]">
+		<div
+			style:height="442px"
+			style:width="486px"
+			class="mt-[19px] ml-[47px] opacity-25 grid justify-center items-center"
+		>
+			
+		</div>
+	</div> -->
+</div>
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,2 +1,3 @@
 export { default as LegionBoard } from './LegionBoard/LegionBoard.svelte'
+export { default as LegionUI } from './LegionUI/LegionUI.svelte'
 export { default as PlacedPiece } from './PlacedPiece/PlacedPiece.svelte'
